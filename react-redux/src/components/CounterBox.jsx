a6658b0f7593ab4fc993ee8f0c358744468ce31c
HJ S <hjseong1222@gmail.com>
2023-05-11 12:44:16 +0900

    0511 리액트 리덕스 기초-4

--- react-redux/src/components/CounterBox.jsx
+++ react-redux/src/components/CounterBox.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 // 리듀서에 있는 값을 수정 : useDispatch
 import { useDispatch, useSelector } from "react-redux"
 
+// dispatch에서 사용할 액션 함수
+import { increase } from '../modules/counter';
+
 export default function CounterBox() {
   //useSelector로 값 가져오기
   // state는 modules의 index.js에서 작성한 rootReducer
@@ -11,7 +14,20 @@ export default function CounterBox() {
   // reducer 함수만 전달해도 useSelector에서 자동으로 state를 뽑아줌
   const counter = useSelector((state)=>(state.counter));
 
+  // useDispatch()를 통해서 사용할 함수를 들고옴
+  const dispatch = useDispatch();
+
   return (
-    <div>{counter}</div>
+    <div>
+        <h3>{counter}</h3>
+        <button onClick={()=>{
+            // 값을 수정하기 위해서, dispatch와 액션함수 사용
+            // 액션함수는 return 값이 객체인 함수 : reducer에 작성
+            // increase() 으로 작성해서 실행하는 이유 : return값 전달을 위해
+            dispatch( increase() )
+        }}>
+            +1
+        </button>
+    </div>
   )
 }
