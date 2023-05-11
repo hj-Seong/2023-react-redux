ce3c404f309ab65afb48ce89f6d1b9bd0823a6ba
HJ S <hjseong1222@gmail.com>
2023-05-11 15:40:21 +0900

    0511 리액트 리덕스 기초-연습확인

--- react-redux/src/components/ExConterBox.jsx
+++ react-redux/src/components/ExConterBox.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increase } from '../modules/counter';
 
 export default function ExConterBox() {
+  const counter = useSelector((state)=>(state.counter))
+  const dispatch = useDispatch();
+
   return (
     <div>
         <p>CounterBox에서 한 내용 복습</p>
         <p>useSelector를 이용하여 값 가져오기</p>
-        <h3>{}</h3>
-        <button>+1</button>
-        <button>-1</button>
+        <h3>{counter}</h3>
+        <button onClick={()=>{ dispatch( increase() ) }}>+1</button>
+                            {/** dispatch에 들어갈 값은 액션 객체이다  */}
+        <button onClick={()=>{ dispatch( {type :"DECREASE"} )}}>-1</button>
         <p>useDispatch를 이용하여 값 수정하기</p>
 
     </div>
