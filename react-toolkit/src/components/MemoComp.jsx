78cdbb1dda66eb3514cd1e08d13d0ca9492bfe9f
HJ S <hjseong1222@gmail.com>
2023-05-15 16:02:36 +0900

    0515 리덕스 툴킷 배열 추가 push

--- react-toolkit/src/components/MemoComp.jsx
+++ react-toolkit/src/components/MemoComp.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { addMemoRedux } from '../slices/memoSlice';
+import { addMemoRedux, addMemoToolkit } from '../slices/memoSlice';
 
 export default function MemoComp() {
   const memo = useSelector((state)=>(state.memo));
+  const [input, setInput] = useState("")
+
   const dispatch = useDispatch()
   return (
     <div>
@@ -15,8 +17,17 @@ export default function MemoComp() {
                 </div>
             ))
         }
-        <button onClick={()=>{dispatch( addMemoRedux({text:"추가", date :"2023-05-15"}) )}}>
+        {/**
+         * 1. memo redux 내용 확인 후, slice 와 비교
+         * 2. input태그를 이용해서 입력받아와서 수정하기
+         *  >> 3시50분에 같이 확인!
+         */}
+        <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+        <button onClick={()=>{ dispatch( addMemoRedux({text:input, date :"2023-05-15"}) )}}>
             메모추가
+        </button>
+        <button onClick={()=>{dispatch( addMemoToolkit({text:input, date:"2023-05-15"}))}}>
+            Toolkit형태로 추가
         </button>
     </div>
   )
--- react-toolkit/src/slices/memoSlice.js
+++ react-toolkit/src/slices/memoSlice.js
@@ -28,7 +28,17 @@ export const memoSlice = createSlice({
             return newMemoList;
         },
         // 값을 직접 수정해서 쓰는 방법
-        addMemoToolkit : (state,action)=>{},
+        addMemoToolkit : (state,action)=>{
+            // 배열의 메소드중 push()
+            // push()는 원본 배열에 값을 추가해서 사용한다
+            const newMemo = {
+                ...action.payload,
+                id : id
+            }
+            state.push(newMemo)
+        },
+        // 이름은 다르지만 위와 동일한 메소드를 만들고
+        // push가 잘 작동하는지 확인해보기
     }
 })
 
@@ -37,5 +47,5 @@ export const memoSlice = createSlice({
 let id = 2;
 
 
-export const { addMemoRedux } = memoSlice.actions
+export const { addMemoRedux, addMemoToolkit } = memoSlice.actions
 export default memoSlice.reducer
