bda713c997c5334ec3c2fc5b16f51480a8757af8
HJ S <hjseong1222@gmail.com>
2023-05-17 11:02:16 +0900

    0517 과제내용 redux툴킷 add/delete

--- 0516-react-test/src/components/MemoComp.jsx
+++ 0516-react-test/src/components/MemoComp.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addMemo, deleteMemo } from '../slices/memoSlice';
 
 export default function MemoComp() {
   const memolist = useSelector((state)=>(state.memo))
+  const [input, setInput] = useState("");
+
+  const dispatch = useDispatch();
   return (
     <div>
         <h2>메모장입니다</h2>
+        <input type="text" 
+            value={input} 
+            onChange={(e)=>{setInput(e.target.value)}}/>
+        <button onClick={()=>{dispatch(addMemo(input))}}>
+            추가
+        </button>
         {
-            memolist.map((memo)=>(
-                <div>
+            memolist.map((memo,index)=>(
+                <div key={memo.id}>
                     <h3>{memo.text}</h3>
                     <span>{memo.date}</span>
                     <button>{memo.isLike ? "찜완료" : "찜하기"}</button>
-                    <button>X</button>
+                    <button 
+                    onClick={()=>{dispatch(deleteMemo(index))}}>
+                        X
+                    </button>
                 </div>
             ))
         }
--- 0516-react-test/src/slices/memoSlice.js
+++ 0516-react-test/src/slices/memoSlice.js
@@ -18,8 +18,34 @@ export const memoSlice = createSlice({
             isLike : true
         },
     ],
-    reducers : {}
+    reducers : {
+        addMemo : (state, action)=>{
+            // 값을 컴포넌트에서 들고올때는 action을 통해서 들고옴
+            // action의 payload로 들고올 값 지정 : text값을 가져옴
+            // id, date, isLike는 reducer에서 값 고정
+            const newMemo = {
+                text : action.payload,
+                id : id,
+                date : "2023-05-17",
+                isLike : false
+            }
+            id++;
+
+            // 리덕스 툴킷은 값에 직접 접근해서 수정해도 
+            // 값이 업데이트가 된다
+            // push()는 원본 배열에 바로 추가
+            // concat()은 원본은 그대로 있고 return 값에 추가
+            state.push(newMemo);
+        },
+        deleteMemo : (state, action)=>{
+            // action.payload : 배열의 인덱스의 값
+            state.splice(action.payload, 1)
+        },
+    }
 })
 
-export const {} = memoSlice.actions;
+// 코드안에서 변수로 쓸 값
+let id = 3;
+
+export const { addMemo, deleteMemo } = memoSlice.actions;
 export default memoSlice.reducer
